c542e133d1f62648308bacd3887f6f36aaa62612
Konrad Gorzelańczyk <61950289+kGorze@users.noreply.github.com>
2025-08-07 23:48:07 +0200

    0.0.11 - optional dependency

--- src/pmarlo/_version.py
+++ src/pmarlo/_version.py
@@ -16,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.0.10.dev1+g3e81670.d20250807"
-__version_tuple__ = version_tuple = (0, 0, 10, "dev1", "g3e81670.d20250807")
+__version__ = version = "0.0.10.dev2"
+__version_tuple__ = version_tuple = (0, 0, 10, "dev2")
