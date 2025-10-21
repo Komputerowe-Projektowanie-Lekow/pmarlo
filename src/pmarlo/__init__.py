bb5bd4dd5352b1c9b1d09a2fbdeb80ee7f74cc0a
Konrad Gorzelańczyk <61950289+kGorze@users.noreply.github.com>
2025-10-21 22:35:46 +0200

    1.0.3 - dynamic versioning

--- src/pmarlo/__init__.py
+++ src/pmarlo/__init__.py
@@ -15,7 +15,11 @@ from typing import Any, Dict, Tuple
 
 from .utils.seed import quiet_external_loggers
 
-__version__ = "0.1.0"
+try:
+    from ._version import __version__
+except ImportError:  # pragma: no cover
+    __version__ = "0.0.0.dev0"
+
 __author__ = "PMARLO Development Team"
 
 _MANDATORY_EXPORTS: Dict[str, Tuple[str, str]] = {
