d0ca6bac6a4c3fef7aba7d4d7f9dc549fdd2315d
Konrad Gorzelańczyk <61950289+kGorze@users.noreply.github.com>
2025-10-21 22:35:46 +0200

    1.0.3 - dynamic versioning

--- build/lib/pmarlo/_version.py
+++ build/lib/pmarlo/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev5'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev5')
+__version__ = version = '0.1.1.dev9'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev9')
 
-__commit_id__ = commit_id = 'g1710a7e30'
+__commit_id__ = commit_id = 'g80f2afdbc'
